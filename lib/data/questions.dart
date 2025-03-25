bf5b6ab2eb91b27e2e7bed96bc8719857fda308c
Jerry Weng <mcjerry168@gmail.com>
2025-03-24 23:18:57 -0400

    created question model and questions

---
+++ lib/data/questions.dart
@@ -0,0 +1,73 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion('What is my favorite color?', [
+    'Green',
+    'Red',
+    'Blue',
+    'Fuchsia',
+  ]),
+
+  QuizQuestion('Which planet is known as the Red Planet?', [
+    'Mars',
+    'Venus',
+    'Jupiter',
+    'Mercury',
+  ]),
+
+  QuizQuestion('What is the capital of Japan?', [
+    'Tokyo',
+    'Kyoto',
+    'Osaka',
+    'Hiroshima',
+  ]),
+
+  QuizQuestion('Who painted the Mona Lisa?', [
+    'Leonardo da Vinci',
+    'Vincent van Gogh',
+    'Pablo Picasso',
+    'Michelangelo',
+  ]),
+
+  QuizQuestion('Which animal is known as the King of the Jungle?', [
+    'Lion',
+    'Tiger',
+    'Elephant',
+    'Gorilla',
+  ]),
+
+  QuizQuestion('What is the largest ocean on Earth?', [
+    'Pacific Ocean',
+    'Atlantic Ocean',
+    'Indian Ocean',
+    'Arctic Ocean',
+  ]),
+
+  QuizQuestion('Which country is home to the kangaroo?', [
+    'Australia',
+    'New Zealand',
+    'South Africa',
+    'Brazil',
+  ]),
+
+  QuizQuestion('What is the main ingredient in guacamole?', [
+    'Avocado',
+    'Tomato',
+    'Cucumber',
+    'Onion',
+  ]),
+
+  QuizQuestion('Which element has the chemical symbol "O"?', [
+    'Oxygen',
+    'Gold',
+    'Osmium',
+    'Oganesson',
+  ]),
+
+  QuizQuestion('Who wrote the Harry Potter book series?', [
+    'J.K. Rowling',
+    'Stephen King',
+    'George R.R. Martin',
+    'Suzanne Collins',
+  ]),
+];
